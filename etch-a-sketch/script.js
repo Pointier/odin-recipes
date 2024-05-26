eccd193d463db568e8ea81983f0006405c23279c
Hugo <hugo.pointier@i2bc.paris-saclay.fr>
2024-05-27 00:43:16 +0200

    etch done, need to do random color and darkening effects

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,14 +1,29 @@
-let gridSize = 16;
-
-const container = document.querySelector("#container");
-
-for (let i = 0; i < gridSize; i++) {
-  const row = document.createElement("div");
-  row.classList.add("row");
+let gridSize = 64;
+function drawingGrid(GgridSize) {
+  const container = document.querySelector("#container");
   for (let i = 0; i < gridSize; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    row.appendChild(square);
+    const row = document.createElement("div");
+    row.classList.add("row");
+    for (let i = 0; i < gridSize; i++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      row.appendChild(square);
+      square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "blue";
+      });
+    }
+    container.appendChild(row);
   }
-  container.appendChild(row);
 }
+
+drawingGrid(gridSize);
+
+button = document.querySelector("button");
+button.addEventListener("click", () => {
+  const rows = Array.from(document.getElementsByClassName("row"));
+  rows.forEach((row) => {
+    row.remove();
+  });
+  gridSize = prompt("Specify a grid size:");
+  drawingGrid(gridSize);
+});
